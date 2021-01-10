6618ef4b523b5df4fe34917a9cb52ca328f9223b
hansenc23 <hansenc216@gmail.com>
2021-01-11 00:40:07 +1100

    style header

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,16 +1,24 @@
 import styles from './styles.module.scss';
-import logo from '../../public/svg/er-logo.svg';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className={styles.nav_container}>
-      <div>
-        <img src={logo} alt='' />
-      </div>
-      <div>
-        <button>Contact Us</button>
-        <button>Menu</button>
+    <div className={styles.header}>
+      <div className={styles.nav_container}>
+        <div>
+          <Link href='/'>
+            <a>
+              <img src='/svg/er-logo.svg' alt='' />
+            </a>
+          </Link>
+        </div>
+        <div>
+          <button>Contact Us</button>
+          <button>Menu</button>
+        </div>
       </div>
     </div>
   );
 };
+
+export default Navbar;
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,12 @@
+import Navbar from '../../components/Navbar';
+
 const Home = () => {
-  return <h1>This is home</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1>This is home</h1>
+    </>
+  );
 };
 
 export default Home;
